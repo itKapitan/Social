7d7c1629afd73aab3418ef0cfa84dd3621c8fe50
itKapitan <itkapitan@gmail.com>
2021-07-04 19:02:36 +0300

    get users from api

--- src/components/Main/Users/Users.jsx
+++ src/components/Main/Users/Users.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import s from "./style.module.css"
+import s from "./style.module.scss"
 import Button from "../../../elements/buttons/Button";
+import * as axios from "axios";
 
-export default function Users({users,unFollow,follow}) {
-    let usersList =  users.map(({id,avatar,name,followed}) => {
+import noAvatar from "../../../assets/images/noavatar.png";
+
+
+export default function Users({users, unFollow, follow, setUsers}) {
+
+    if (users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            setUsers(response.data.items);
+        });
+
+    }
+    let usersList = users.map(({id, photos, name, followed}) => {
         return (
             <>
                 <div className={s.wr} key={id}>
                     <div className={s.avatar}>
-                        <img
-                            src={avatar}
-                            alt={name}/>
+                        { photos.small ?
+                            <img src={photos.small} alt={name}/> :
+                            <img src={noAvatar} alt={name}/>
+                        }
                     </div>
                     <div className={s.info}>
                         <div className={s.name}>
@@ -19,9 +31,13 @@ export default function Users({users,unFollow,follow}) {
                         <div className={s.buttons}>
                             <Button Text={'Написать сообщение'} Style={'default'}/>
                             {followed ?
-                                <Button onClick={ ()=>{unFollow(id)} } Text={'Удалить из друзей'} Style={'default'} mstyle={s.btn}/>
+                                <Button onClick={() => {
+                                    unFollow(id)
+                                }} Text={'Удалить из друзей'} Style={'default'} mstyle={s.btn}/>
                                 :
-                                <Button onClick={ ()=>{follow(id)} } Text={'Добавить в друзья'} Style={'default'} mstyle={s.btn}/>
+                                <Button onClick={() => {
+                                    follow(id)
+                                }} Text={'Добавить в друзья'} Style={'default'} mstyle={s.btn}/>
                             }
                         </div>
                     </div>
@@ -30,13 +46,12 @@ export default function Users({users,unFollow,follow}) {
             </>
         );
     });
-    return(
+    return (
         <>
             {usersList}
             <Button Text={'Показать ещё'} Style={'default'}/>
         </>
     )
-
 
 
 }
